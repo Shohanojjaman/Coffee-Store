fabf16268d25ced3d5ee328d7916e375624ad776
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-10-14 16:16:54 +0600

    Import Rancho and Raleway fonts form google fonts.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Rancho: "'Rancho', cursive",
+        Raleway: "'Raleway', sans-serif",
+      },
+    },
   },
   plugins: [],
 };
