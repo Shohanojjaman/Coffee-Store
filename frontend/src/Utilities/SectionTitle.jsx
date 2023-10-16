a1e7afa6d3702e20c60bb8a6b0ce69c6db9e7f88
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-10-16 19:26:00 +0600

    Fixed a issue.

--- frontend/src/Utilities/SectionTitle.jsx
+++ frontend/src/Utilities/SectionTitle.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 const SectionTitle = ({ title, subTitle }) => {
   return (
     <div className="text-center">
-      <h6 className="text-xl">{subTitle}</h6>
+      <p className="text-xl">{subTitle}</p>
       <h4 className="text-theme text-5xl drop-shadow-[5px_5px_5px_rgba(51,26,21,0.6)]">{title}</h4>
     </div>
   );
